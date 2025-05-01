ed29edcb6e46e499d02e4d1bf4548155ecdc63a7
Anup Mishra <anupm0873@gmail.com>
2025-05-02 00:59:54 +0530

    add footer

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,10 +1,12 @@
 import TestimonialsCarousel from "../components/Testimonials";
 import WallOfLove from "../components/walloflove";
+import Footer from "../components/footer";
 const LandingPage = () => {
     return (
         <div className="bg-gray-900 text-white">
           <TestimonialsCarousel/>
           <WallOfLove/>
+          <Footer/>
         </div>
       );
     };
