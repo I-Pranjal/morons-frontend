fa04c7a8704930eb94af4a80b9e239feb91d327d
Pranjal <prathammmishra947@gmail.com>
2025-05-02 01:13:57 +0530

    Structured the project

---
+++ src/components/landinganoop.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingAnoop() {
+  return (
+    <div>
+             <h1 className='bg-amber-50'>Your are viewing this content from Anoop's Component</h1>
+    </div>
+  )
+}
+
+export default LandingAnoop
