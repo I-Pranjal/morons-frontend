4fbfdaa4982b962e6baff299586a05139f6d96ca
Pranjal <prathammmishra947@gmail.com>
2025-05-02 17:58:10 +0530

    Correctd wall of love address

--- src/components/landinganoop.jsx
+++ src/components/landinganoop.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Testimonials from './Testimonials'
-import WallofLove from './WallofLove'
+import WallofLove from './walloflove'
 import Footer from './footer';
 import { toast } from 'react-toastify';
 
