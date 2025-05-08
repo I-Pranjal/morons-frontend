d5d89e620a8df24d2016cd7f4d357b130484ae75
Pranjal <prathammmishra947@gmail.com>
2025-05-08 11:50:17 +0530

    Added address change

--- src/components/SchedulingWorkflow.jsx
+++ src/components/SchedulingWorkflow.jsx
@@ -7,7 +7,7 @@ import {
   CardTitle
 } from "../components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "../components/ui/Button";
+import { Button } from "../components/ui/button";
 import { Progress } from "../components/ui/progress";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../components/ui/tabs";
 import { 
