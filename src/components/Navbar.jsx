634d5ad90de870a5117b5fb3e6f169cd9e1dc271
Pranjal <prathammmishra947@gmail.com>
2025-05-08 11:45:42 +0530

    Updated button address

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from './ui/Button';
+import { Button } from './ui/button';
 import logo from '../assets/logo.png';
 
 const ChevronDown = () => (
--- src/components/landingrishabh.jsx
+++ src/components/landingrishabh.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Hero from './Hero'
-import BookingExperience from './BookingExperience'
-import SchedulingWorkflow from './SchedulingWorkflow'
-import CustomBookingSection from './CustomBookingSection'
-import FeatureSection from './FeatureSection'
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import BookingExperience from './BookingExperience';
+import SchedulingWorkflow from './SchedulingWorkflow';
+import CustomBookingSection from './CustomBookingSection';
+import FeatureSection from './FeatureSection';
 
 function LandingRishabh() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-      <div className="min-h-screen bg-gray-50">
-       <Navbar/>
-       <main className="container mx-auto px-4 py-8">
+    <div
+      className={`min-h-screen ${
+        darkMode ? 'bg-[#121212] text-white' : 'bg-gray-50 text-black'
+      }`}
+    >
+      <Navbar />
+      <button
+        onClick={() => setDarkMode(!darkMode)}
+        className="absolute top-4 right-4 px-4 py-2 bg-blue-500 text-white rounded"
+      >
+        Toggle Dark Mode
+      </button>
+      <main className="container mx-auto px-4 py-8">
         <Hero />
         <SchedulingWorkflow />
-        <CustomBookingSection/>
-        <BookingExperience/>
-        <FeatureSection/>
-       </main>
+        <CustomBookingSection />
+        <BookingExperience />
+        <FeatureSection />
+      </main>
     </div>
-  )
+  );
 }
 
-export default LandingRishabh
+export default LandingRishabh;
