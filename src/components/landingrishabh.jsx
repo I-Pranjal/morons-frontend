cc37669ee97947a8028b39c939283b19ae4fa1f5
Pranjal <prathammmishra947@gmail.com>
2025-05-08 11:55:11 +0530

    chagnes

--- src/components/landingrishabh.jsx
+++ src/components/landingrishabh.jsx
@@ -16,12 +16,6 @@ function LandingRishabh() {
       }`}
     >
       <Navbar />
-      <button
-        onClick={() => setDarkMode(!darkMode)}
-        className="absolute top-4 right-4 px-4 py-2 bg-blue-500 text-white rounded"
-      >
-        Toggle Dark Mode
-      </button>
       <main className="container mx-auto px-4 py-8">
         <Hero />
         <SchedulingWorkflow />
